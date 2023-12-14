6ad71dd37352fce35c0e8d5e86abd6b4ed57b8e8
Narutchai <koonfluk2015@gmail.com>
2023-12-14 16:21:53 +0700

    update Cartpage

--- client/src/page/CartPage.jsx
+++ client/src/page/CartPage.jsx
@@ -8,46 +8,57 @@ const CartPage = () => {
   const [data, setData] = useState();
 
   useEffect(() => {
-    const fecthData = async () => {
-      try {
-        const res = await axiosInstance.get(
-          `/api/getBookinCart?userID=${userInfo.id}`
-        );
-        setData(res.data.matching);
-        // console.log(data);
-      } catch (err) {
-        console.log(err);
-      }
+    const fetchData = async () => {
+      const res = await axiosInstance.get(
+        `/api/getBookinCart?userID=${userInfo.id}`
+      );
+      setData(res.data.matching);
     };
-    fecthData();
-  }, [userInfo]);
+    fetchData();
+  }, [userInfo.id]);
 
-  console.log(data);
-
-  const showData = () => {
-    if (data) {
-      return data.map((item) => {
-        return item.bookInfo?.map((book) => {
-          return (
-            <div key={book._id} className="card-item">
-              <img src={book.image} alt="" />
-              <h1>{book.name}</h1>
-              <h1>{book.price}</h1>
-              <button onClick={
-                () => 
-                axiosInstance.delete(`/api/deleteBookinCart?id=${item._id}`)
-              }>remove</button>
-            </div>
-          );
+  const handleDelete = async (bookID) => {
+    try {
+      await axiosInstance
+        .delete(`/api/deleteBookInCart?id=${bookID}`)
+        .then(() => {
+          window.location.reload();
         });
-      });
+    } catch (error) {
+      console.log(error);
     }
   };
+
+  const showData = data?.map((item) => {
+    return item.bookInfo.map((book) => {
+      return (
+        <div key={book._id} className="card-item">
+          <img src={book.image} alt={book.title} />
+          <p>{book.title}</p>
+          <p>{book.price}</p>
+          <button onClick={() => handleDelete(item._id)}>Delete</button>
+        </div>
+      );
+    });
+  });
+
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    data?.forEach((item) => {
+      item.bookInfo.forEach((book) => {
+        totalPrice += parseInt(book.price);
+      });
+    });
+    return totalPrice;
+  };
+
+
 
   return (
     <>
       <h1>CART PAGE</h1>
-      {showData()}
+      {showData}
+      {calculateTotalPrice()}
     </>
   );
 };
