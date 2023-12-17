a566a2af049fd07201201aee3d1762f3ccb660cb
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2023-12-18 04:48:39 +0700

    updatecontroller

--- serverdb/src/controller/Admin/UpdateBookController.ts
+++ serverdb/src/controller/Admin/UpdateBookController.ts
@@ -4,10 +4,27 @@ import { reportError , getErrorMessage } from '../../lib/Error';
 
 export const UpdateBook = async (req: Request , res: Response ) => {
     try {
-        const { id , title , price } = req.body;
+        const { id } = req.params;
+        const { title , price } = req.body;
         const client = await dbConnect();
-        await client.query(`UPDATE FROM Comic SET title = ? , price = ? WHERE .id = ?` , { title , price , id});
-        return res.status(201).send({
+        
+        if ( title === "" && price === "") {
+            return res.status(400).send({ message: "Please fill all required field" });
+        }
+        else if ( title === undefined && price === undefined) {
+            return res.status(400).send({ message: "Please fill all required field" });
+        }
+        else if ( title === null && price === null) {
+            return res.status(400).send({ message: "Please fill all required field" });
+        } else if ( title && price ){
+            await client.query(`UPDATE FROM Comic SET title = "${title}" , price = ${price} WHERE .id = ${id}`);
+        } else if ( title ) {
+            client.query(`UPDATE FROM Comic SET title = "${title}" WHERE .id = ${id}`);
+        } else if ( price ){
+            client.query(`UPDATE FROM Comic SET price = ${price} WHERE .id = ${id}`);
+        }
+        
+        res.status(200).send({
             message: "Update comic successed"    
         })
     } catch (error) {
